d69d7961383a21dea0556a8a0e6401a66c52cc44
ViacheslavP <public.viacheslav@gmail.com>
2021-06-18 19:57:14 +0300

    Added sigma_matrix.py

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 import numpy as np
 from src.radiative_shift import HexagonModel
-
+from src.radiative_shift import markovianSigmaMatrixForV
 
 # Теперь у нас есть доступ к нашим атомам здесь
+
+
+model = HexagonModel(3, 5)
+
+gammas, _ = np.linalg.eig(markovianSigmaMatrixForV(model))
+
+# По идее здесь ошибка - не должно быть положительных значений, ошибка в markovian. Буду проверять.
+print(np.sort(np.imag(gammas)))
---
+++ src/radiative_shift/dyson_solvers/__init__.py
@@ -0,0 +1,1 @@
+from .sigma_matrix import markovianSigmaMatrixForV
