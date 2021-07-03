5d2dea74e258b55d2c683f99f1d87f67bd5ba436
viacheslav <public.viacheslav@gmail.com>
2021-07-04 00:30:03 +0300

    added density test

--- tests/hexagon_sphere.py
+++ tests/hexagon_sphere.py
@@ -1,11 +1,13 @@
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from src.radiative_shift import HexagonSphere
+from src.radiative_shift.model.param import SPHERE_RADIUS
+import numpy as np
 
 layers = 7
 copies = 11
 
-
+# Tessting execution
 test = HexagonSphere(layers, copies)
 
 x = test.x
@@ -17,5 +19,18 @@ ax = plt.axes(projection='3d')
 ax.scatter3D(x, y, z)
 plt.show()
 
-ax, ay, az, ar = test.getDistances()  # Возвращает 4 матрицы np.array
-print(ax)
+# ax, ay, az = test.getDistances()  # Возвращает 4 матрицы np.array
+# print(ax)
+
+# Testing density
+RADIUS = SPHERE_RADIUS
+factors = np.linspace(0.9, 1.5, 100)
+density = []
+natoms = []
+for factor in factors:
+    model = HexagonSphere(layers, copies, RADIUS * factor)
+    density.append(len(model.x) / 4 / np.pi * 3 / (RADIUS * factor)**3)
+    natoms.append(len(model.x))
+
+plt.plot(factors, density)
+plt.show()
